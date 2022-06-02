eead258fec58c5406fbc6e7f4770c2d33caccf2b
bissaye <bissaye@ce50aa11-1356-8b40-bb49-45a9e40b6d73>
2022-06-02 16:22:15 +0000

    fin
    git-svn-id: https://desktop-kof97jq/svn/DEMO/branches/modif2@6 ce50aa11-1356-8b40-bb49-45a9e40b6d73

--- script.sh
+++
@@ -1,8 +0,0 @@
-#firstly we clone the svn repo
-git svn clone -T $branche $svnRepoLink
-
-#we switch on the specific branche
-git checkout -b $brancheName
-
-#we push the project to the repo
-git push $repoLink
